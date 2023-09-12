b3d2c9c0f7ffc4d7bbed1b8a003465c3bf27dbbb
petekp <pete.petrash@gmail.com>
2023-09-12 11:18:36 -0700

    layout improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import type {
   InferGetServerSidePropsType,
 } from "next";
 import { getProviders, signIn, signOut, getSession } from "next-auth/react";
+import { useEffect } from 'react';
 
 import { Button } from "@/components/ui/button";
 
@@ -12,6 +13,53 @@ import AccountView from "./components/AccountView";
 import Logo2 from "./components/Logo2";
 import ThemeToggle from "./components/ThemeToggle";
 import Logo from "./components/Logo";
+import { motion, type AnimationProps } from 'framer-motion';
+
+const approxDiamondAnimLength = 2
+
+const diamondAnimation: AnimationProps = {
+  initial: { opacity: 0, scale: 0.85, rotate: 45 },
+  animate: { opacity: 1, scale: 1, rotate: 45, transition: { type: 'spring', damping: 20, stiffness: 20 } }
+};
+
+const logoAnimation: AnimationProps = {
+  initial: { opacity: 0, scale: 0.9,  },
+  animate: { opacity: 1, scale: 1,  transition: { type: 'spring', damping: 10, stiffness: 30, delay: approxDiamondAnimLength } }
+};
+
+const letterAnimation: AnimationProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { type: 'spring', damping: 40, stiffness: 150 } }
+};
+
+const staggerAnimation: AnimationProps = {
+  initial: 'hidden',
+  animate: 'visible',
+  variants: {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        damping: 50,
+        stiffness: 50,
+        delayChildren: 1,
+        staggerChildren: 0.05
+      }
+    }
+  }
+};
+
+const descriptionAnimation: AnimationProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { delay: approxDiamondAnimLength, type: 'spring', damping: 20, stiffness: 150 } }
+};
+
+const signInAnimation: AnimationProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { delay: approxDiamondAnimLength + 2, type: 'spring', damping: 20, stiffness: 150 } }
+};
+
 
 export default function Home({
   providers,
@@ -20,10 +68,10 @@ export default function Home({
   return (
     <>
       <Head>
-        <title>Diagrammaton - AI Powered Diagramming for FigJam</title>
+        <title>Diagrammaton - AI Powered diagrams for FigJam</title>
         <meta
           name="description"
-          content="Diagrammaton - AI Powered Diagramming for FigJam"
+          content="Diagrammaton - AI Powered diagrams for FigJam"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -31,7 +79,9 @@ export default function Home({
         <div className="absolute flex h-full max-h-full w-full max-w-full items-center justify-center overflow-hidden align-middle opacity-50">
           <Logo />
         </div>
+         <motion.div {...diamondAnimation} className="absolute w-[450px] h-[450px] rounded-sm  border-accent-foreground border backdrop-blur-md rotate-45 origin-center z-auto"></motion.div>
         <SignIn providers={providers} sessionData={sessionData} />
+
       </main>
       <div className="absolute right-0 top-0 p-4">
         <ThemeToggle />
@@ -46,29 +96,36 @@ function SignIn({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="container relative mx-auto w-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
-      <div className="lg:p-8">
+
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 ">
           <div className="mx-auto flex flex-col space-y-2 text-center ">
-            <div className="w-[450px] space-y-5 rounded-md border border-accent-foreground pb-0 pt-7 backdrop-blur-md">
+
+            <div className="relative w-[450px] space-y-5 pb-0 pt-7">
               <div className="space-y-10">
                 <div className="select-none space-y-5">
-                  <div className="flex items-center justify-center align-middle">
+                  <motion.div {...logoAnimation} className="flex items-center justify-center align-middle">
                     <Logo2 />
-                  </div>
+                  </motion.div>
                   <div className="space-y-1">
-                    <div className="text text-lg font-bold uppercase tracking-widest">
-                      Diagrammaton
-                    </div>
-                    <div className="text text-muted">
-                      AI powered Diagramming for FigJam
-                    </div>
+
+                      <motion.div {...staggerAnimation} className="text text-lg font-extrabold uppercase tracking-widest text-primary">
+                        {"Diagrammaton".split("").map((char, index) => (
+                          <motion.span key={index} {...letterAnimation} variants={staggerAnimation.variants}>
+                            {char}
+                          </motion.span>
+                        ))}
+                      </motion.div>
+
+                    <motion.div {...descriptionAnimation} className="text text-muted">
+                      AI powered diagrams for FigJam
+                    </motion.div>
                   </div>
                 </div>
                 {sessionData && <AccountView />}
-                <div className="flex flex-1 flex-row space-y-2 border-t border-accent-foreground px-5">
+                <div className="flex flex-1 flex-col px-5">
                   {!sessionData && (
-                    <>
-                      <div className="text-sm text-muted-foreground">
+                    <motion.div {...signInAnimation} className="space-y-2">
+                      <div className="text-sm text-muted select-none">
                         Sign in to get a license code
                       </div>
                       {Object.values(providers).map((provider) => (
@@ -81,7 +138,7 @@ function SignIn({
                           </Button>
                         </div>
                       ))}
-                    </>
+                    </motion.div>
                   )}
                   {sessionData && (
                     <div className="flex flex-1 select-none flex-row items-center justify-between gap-2 py-1 ">
@@ -100,6 +157,7 @@ function SignIn({
                 </div>
               </div>
             </div>
+
           </div>
 
           {/* <p className="px-8 text-center text-sm text-muted-foreground">
@@ -120,7 +178,8 @@ function SignIn({
 
           </p> */}
         </div>
-      </div>
+
+
     </div>
   );
 }
