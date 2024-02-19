94625179a96333f5bd082395eea3ade408395449
Pete Petrash <pete.petrash@gmail.com>
2024-02-19 11:30:55 -0600

    remove example route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,20 +1,12 @@
 import { diagrammatonRouter } from "./routers/diagrammaton";
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { apiKeyRouter } from "./routers/apikey";
 import { licenseKeyRouter } from "./routers/license";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   apiKey: apiKeyRouter,
   license: licenseKeyRouter,
   diagrammaton: diagrammatonRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
