dcdf864069419835ed0a769438a00777428d8956
Pete Petrash <pete.petrash@gmail.com>
2024-02-19 11:31:21 -0600

    create prompt refinements

--- src/plugins/diagrammaton/lib.ts
+++ src/plugins/diagrammaton/lib.ts
@@ -8,18 +8,21 @@ import {
 } from "~/app/types";
 
 const shapes = [
-  "ROUNDED_RECTANGLE",
-  "ELLIPSE",
-  "DIAMOND",
   "SQUARE",
+  "ELLIPSE",
+  "ROUNDED_RECTANGLE",
+  "DIAMOND",
   "TRIANGLE_UP",
   "TRIANGLE_DOWN",
-  "ENG_DATABASE",
   "PARALLELOGRAM_RIGHT",
   "PARALLELOGRAM_LEFT",
+  "ENG_DATABASE",
+  "ENG_QUEUE",
+  "ENG_FILE",
+  "ENG_FOLDER",
 ];
 
-const magnet = ["TOP", "RIGHT", "BOTTOM", "LEFT"];
+const magnetPositions = ["TOP", "RIGHT", "BOTTOM", "LEFT"];
 
 const nodeSchema = {
   type: "object",
@@ -34,26 +37,35 @@ const nodeSchema = {
     },
     label: {
       type: "string",
-      description: "The label for the diagram node.",
+      description:
+        "A concise label for the diagram node. If it's a decision point, you must print the label as a question.",
     },
     shape: {
       type: "string",
       enum: shapes,
-      description:
-        "The shape for the diagram node. Must be one of the provided enum values.",
+      description: `The shape for the diagram node. Must be one of the provided enum values. Use the following guide for choosing a shape based on the node content: 
+        
+        - Decision: DIAMOND
+        - Action: SQUARE
+        - Input or output: PARALLELOGRAM_RIGHT
+        - Process: ROUNDED_RECTANGLE
+        - Start or end point: ELLIPSE
+        - Database: ENG_DATABASE
+        - Document: ENG_FILE`,
     },
   },
 };
 
 const linkSchema = {
   type: "object",
+  required: ["fromNodeId", "toNodeId", "fromMagnet", "toMagnet"],
   description:
-    "Completely optional, connective link between two nodes taking magnet positions into account. Forward links between adjacent nodes are horizontal. Vertical should be used mostly for backwards links and exceptions. Sometimes it's best not to have a label where a link is obvious!",
+    "A connective link between two nodes taking magnet positions into account. By default, links between adjacent nodes are RIGHT to LEFT. BOTTOM should be used mostly for backwards links and exceptional steps like errors.",
   properties: {
     label: {
       type: "string",
-      description:
-        "A completely optional (can be blank!), very concise label of no more than 2-3 words.",
+      enum: ["Yes", "No"],
+      description: `Optional label for the link. If the link is obvious, it's best not to have a label.`,
     },
     fromNodeId: {
       type: "string",
@@ -65,15 +77,22 @@ const linkSchema = {
     },
     fromMagnet: {
       type: "string",
-      enum: magnet,
-      description:
-        "Magnet position on the origin node where the link originates.",
+      enum: magnetPositions,
+      description: `Magnet position on the origin node where the link originates. Follow these guidelines exactly: 
+        
+        - RIGHT by default.
+        - BOTTOM If the connection is to a prior node.
+        - For decision nodes: RIGHT for "Yes". BOTTOM for "No".`,
     },
     toMagnet: {
       type: "string",
-      enum: magnet,
-      description:
-        "Magnet position on the target node where the link terminates.",
+      enum: magnetPositions,
+      description: `Magnet position on the target node where the link terminates. Follow these guidelines exactly:
+
+        - LEFT by default.
+        - BOTTOM if LEFT is already occupied.
+        - TOP if BOTTOM is already occupied.
+        `,
     },
   },
 };
@@ -90,11 +109,14 @@ export const functions = [
     description: `Translates a diagram description into valid JSON`,
     parameters: {
       type: "object",
+      required: ["steps"],
       properties: {
         steps: {
           type: "array",
+          required: ["items"],
           items: {
             type: "object",
+            required: ["from", "link", "to"],
             properties: {
               from: nodeSchema,
               link: linkSchema,
@@ -104,7 +126,6 @@ export const functions = [
         },
       },
     },
-    required: ["steps"],
   },
   {
     name: "generate_error_message",
@@ -147,11 +168,19 @@ export const createGenerateMessages = (
   return [
     {
       role: "system",
-      content: `You are an advanced AI specialized (but not limited to) generating UI/UX-oriented flow diagrams from simple, user-generated natural language text descriptions. Link labels are not always necessary (blank is fine) but should remain crisp and succinct. If you're unable to generate a useful diagram from the description, print an error that's both witty and helpful; don't apologize or say things like "oops".`,
+      content: `You are an advanced AI specialized (but not limited to) generating UI/UX-oriented flow diagrams from simple, user-generated natural language text descriptions. Based on your deep knowledge of best practices and well-established patterns, ensure the diagram is immaculate, thorough, and exhaustive to the best of your abilities. Keenly anticipate steps not included in the description, particularly error states and edge cases that may not be explicitly mentioned in the diagram description. If the description mentions a particular service or domain, confidently infer what domain-specific steps should be included. Think step by step to ensure the diagram is complete and meets a high standard. My livelihood depends on you being superlatively creative and helpful. If you're unable to generate a useful diagram from the description, print an error that's both witty and helpful; don't apologize or say things like "oops".
+
+      Here is an example of a user description and the expected output:
+
+      Description: A mobile app signup flow
+      Output: [{"from":{"id":"start","label":"Start","shape":"ELLIPSE"},"link":{"fromNodeId":"start","toNodeId":"chooseSignUpMethod"},"to":{"id":"chooseSignUpMethod","label":"Choose Sign Up Method","shape":"ROUNDED_RECTANGLE"}},{"from":{"id":"chooseSignUpMethod","label":"Choose Sign Up Method","shape":"ROUNDED_RECTANGLE"},"link":{"fromNodeId":"chooseSignUpMethod","toNodeId":"email","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"email","label":"Email","shape":"PARALLELOGRAM_RIGHT"}},{"from":{"id":"chooseSignUpMethod","label":"Choose Sign Up Method","shape":"ROUNDED_RECTANGLE"},"link":{"fromNodeId":"chooseSignUpMethod","toNodeId":"socialMedia","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"socialMedia","label":"Social Media","shape":"PARALLELOGRAM_RIGHT"}},{"from":{"id":"email","label":"Email","shape":"PARALLELOGRAM_RIGHT"},"link":{"fromNodeId":"email","toNodeId":"inputEmailPassword","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"inputEmailPassword","label":"Input Email & Password","shape":"SQUARE"}},{"from":{"id":"socialMedia","label":"Social Media","shape":"PARALLELOGRAM_RIGHT"},"link":{"fromNodeId":"socialMedia","toNodeId":"choosePlatform","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"choosePlatform","label":"Choose Platform","shape":"SQUARE"}},{"from":{"id":"inputEmailPassword","label":"Input Email & Password","shape":"SQUARE"},"link":{"fromNodeId":"inputEmailPassword","toNodeId":"validateEmail","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"validateEmail","label":"Valid email?","shape":"DIAMOND"}},{"from":{"id":"validateEmail","label":"Valid email?","shape":"DIAMOND"},"link":{"fromNodeId":"validateEmail","toNodeId":"emailValid","fromMagnet":"RIGHT","toMagnet":"LEFT","label":"Yes"},"to":{"id":"emailValid","label":"Email Valid","shape":"SQUARE"}},{"from":{"id":"validateEmail","label":"Valid email?","shape":"DIAMOND"},"link":{"fromNodeId":"validateEmail","toNodeId":"emailInvalid","fromMagnet":"BOTTOM","toMagnet":"LEFT","label":"No"},"to":{"id":"emailInvalid","label":"Email Invalid","shape":"SQUARE"}},{"from":{"id":"emailInvalid","label":"Email Invalid","shape":"SQUARE"},"link":{"fromNodeId":"emailInvalid","toNodeId":"inputEmailPassword","fromMagnet":"BOTTOM","toMagnet":"BOTTOM"},"to":{"id":"inputEmailPassword","label":"Input Email & Password","shape":"SQUARE"}},{"from":{"id":"choosePlatform","label":"Choose Platform","shape":"SQUARE"},"link":{"fromNodeId":"choosePlatform","toNodeId":"externalAuth","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"externalAuth","label":"External Authentication","shape":"SQUARE"}},{"from":{"id":"emailValid","label":"Email Valid","shape":"SQUARE"},"link":{"fromNodeId":"emailValid","toNodeId":"createAccount","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"createAccount","label":"Create Account","shape":"ROUNDED_RECTANGLE"}},{"from":{"id":"externalAuth","label":"External Authentication","shape":"SQUARE"},"link":{"fromNodeId":"externalAuth","toNodeId":"authSuccessful","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"authSuccessful","label":"Authentication Successful","shape":"SQUARE"}},{"from":{"id":"externalAuth","label":"External Authentication","shape":"SQUARE"},"link":{"fromNodeId":"externalAuth","toNodeId":"authFailed","fromMagnet":"BOTTOM","toMagnet":"TOP"},"to":{"id":"authFailed","label":"Authentication Failed","shape":"SQUARE"}},{"from":{"id":"authFailed","label":"Authentication Failed","shape":"SQUARE"},"link":{"fromNodeId":"authFailed","toNodeId":"chooseSignUpMethod","fromMagnet":"BOTTOM","toMagnet":"BOTTOM"},"to":{"id":"chooseSignUpMethod","label":"Choose Sign Up Method","shape":"ROUNDED_RECTANGLE"}},{"from":{"id":"authSuccessful","label":"Authentication Successful","shape":"SQUARE"},"link":{"fromNodeId":"authSuccessful","toNodeId":"createAccount","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"createAccount","label":"Create Account","shape":"ROUNDED_RECTANGLE"}},{"from":{"id":"createAccount","label":"Create Account","shape":"ROUNDED_RECTANGLE"},"link":{"fromNodeId":"createAccount","toNodeId":"end","fromMagnet":"RIGHT","toMagnet":"LEFT"},"to":{"id":"end","label":"End","shape":"ELLIPSE"}}]
+      
+      
+      `,
     },
     {
       role: "user",
-      content: `Generate an exhaustive diagram from the following description: "${diagramDescription}". Based on your deep knowledge of best practices and well-established patterns, ensure the diagram is thorough & detailed. Keenly anticipate steps not included in the description, particularly error states and similar edge cases that may not be explicitly mentioned. Think step by step to ensure the diagram is complete and meets a high standard.`,
+      content: `Infer an exhaustive diagram based on following description: "${diagramDescription}". `,
     },
   ];
 };
@@ -163,11 +192,11 @@ export const createModifyMessages = (
   return [
     {
       role: "system",
-      content: `You are an advanced AI specialized (but not limited to) modifying existing UI/UX-oriented flow diagram data based on natural language instructions provided by the end user.`,
+      content: `You are an advanced AI specialized in but not limited to modifying existing UI/UX-oriented flow diagram data generated by a FigJam plugin based on natural language instructions provided by the end user. This data will then be fed back to the FigJam plugin so it needs adhere to the plugin's data format. If you're unable to modify the diagram data according to the instructions, print an error that's both witty and helpful; don't apologize or say things like "oops".`,
     },
     {
       role: "user",
-      content: `Modify the following diagram data according to these instructions:\n\nDiagram data: ${diagramData}\n\n Instructions: "${instructions}"\n\n Based on your deep knowledge of best practices and well-established patterns, ensure the diagram is both updated to satisfy the instructions, and re-configured if necessary. Keenly anticipate steps not included in the instructions, particularly error states and similar edge cases that may not be explicitly mentioned. Think step by step to ensure the diagram is complete and meets a high standard. If the instructions are unclear or impossible to follow, print an error that's both witty and helpful; don't apologize or say things like "oops".`,
+      content: `Modify the following diagram data according to these instructions:\n\nDiagram data: ${diagramData}\n\n Instructions: "${instructions}"\n\n Based on your deep knowledge of best practices and well-established patterns, ensure the diagram is faithful to the original, but updated to satisfy the modification instructions. Keenly anticipate steps not included in the instructions, particularly error states and similar edge cases that may not be explicitly mentioned. Think step by step to ensure the diagram is complete and meets a high standard. If the instructions are unclear or impossible to follow, print an error that's both witty and helpful; don't apologize or say things like "oops".`,
     },
   ];
 };
